a2cc03b633a10e0c4ce2e64610348fba158651a9
Filipp <amouniverso@yandex.ru>
2023-02-05 02:26:41 +0300

    tuning

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 'use strict';
 const ccxt = require ('ccxt');
+const chalk = require ('chalk');
 
+const log = console.log
 const createTable = (exchanges, prices, colCount) => {
     const result = []
     for (let i = 0; i < exchanges.length; i += colCount) {
@@ -8,8 +10,9 @@ const createTable = (exchanges, prices, colCount) => {
             exchanges.reduce((accumRow, currRow, indexRow, arrayRow) => {
                 return ({...accumRow, [currRow.id]: arrayRow.slice(i, i + colCount).reduce((accumCol, currCol) => {
                         let priceRatio = prices[currRow.id].price / prices[currCol.id].price
-                        priceRatio = priceRatio <= 1 ? 1 - priceRatio : -(1 - 1/priceRatio)
-                        const priceDiff = currRow.id !== currCol.id ? `${(priceRatio * 100).toFixed(4)}%` : ''
+                        priceRatio = (((priceRatio >= 1 ? priceRatio: 1/priceRatio) - 1) * 100).toFixed(4)
+                        priceRatio = priceRatio > 1 ? priceRatio : '<1%'
+                        const priceDiff = currRow.id !== currCol.id ? `${priceRatio}%` : ''
                         return {...accumCol, [currCol.id]: priceDiff}
                     }, {})})
             }, {})
@@ -19,13 +22,14 @@ const createTable = (exchanges, prices, colCount) => {
 }
 
 (async function () {
-    console.log('============= DeFi combat bot ============')
-    console.log('============= Initializing... ============\n')
+    console.log(chalk.green('============= DeFi combat bot ============'))
+    console.log(chalk.green('============= Initializing... ============\n'))
 
     const unavailableExchanges = ['bitflyer', 'bithumb', 'bitstamp', 'btcalpha', 'buda', 'btcmarkets', 'coinmate',
         'huobijp', 'coinone', 'kuna', 'mercado', 'luno', 'therock', 'tidex', 'ripio', 'yobit', 'zipmex', 'okex',
-        'okx', 'okex5', 'binanceus', 'kraken', 'bitfinex2', 'timex', 'blockchaincom', 'coinbase', 'alpaca']
-    const pair = 'ETH/USDT'
+        'okx', 'okex5', 'binanceus', 'kraken', 'bitfinex2', 'timex', 'blockchaincom', 'coinbase', 'alpaca', 'woo',
+        'hollaex']
+    const pair = 'SAND/USDT'
     let exchanges = ccxt.exchanges.map(ex => {
         return !unavailableExchanges.includes(ex) ? new ccxt[ex]() : null
     }).filter(el => el)
@@ -35,13 +39,23 @@ const createTable = (exchanges, prices, colCount) => {
     console.log('Markets loaded. Num of valid exchanges: ', exchanges.length, '\n')
 
     let sleep = (ms) => new Promise (resolve => setTimeout (resolve, ms));
+    const tickers = await Promise.all(
+        exchanges.map(exchange => exchange.fetchTicker(pair)
+    ))
+    console.log('Quote volume for ' + pair + ':')
+    tickers.forEach((el, index) => {
+        console.log(exchanges[index].id + ':',
+            chalk.green(new Intl.NumberFormat('en-US').format(el.quoteVolume), pair.split('/')[1])
+        )
+    })
     while (true) {
         const trades = await Promise.all(
             exchanges.map(exchange => exchange.fetchTrades (pair, undefined, 1)
         ))
         const prices = {}
         trades.forEach((trade, index) => {
-            const {price, symbol} = trade[trade.length - 1]
+            const lastTrade = trade[trade.length - 1]
+            const {price, symbol} = lastTrade ? lastTrade : {}
             prices[exchanges[index].id] = {price, symbol}
         })
         console.log(pair + ':')
